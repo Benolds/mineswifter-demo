8f4395e929377b664fadad5f9070560cab275e52
Benjamin Reynolds <reynoldsben94@gmail.com>
2014-07-27 13:36:26 -0400

    Fully implemented the Board class

--- Mineswifter/Board.swift
+++ Mineswifter/Board.swift
@@ -27,4 +27,69 @@ class Board: NSObject {
         }
         
     }
+    
+    func resetBoard() {
+        // assign mines to squares
+        for row in 0 ..< size {
+            for col in 0 ..< size {
+                squares[row][col].isRevealed = false
+                self.calculateIsMineLocationForSquare(squares[row][col])
+            }
+        }
+        
+        // count number of neighboring squares
+        for row in 0 ..< size {
+            for col in 0 ..< size {
+                self.calculateNumNeighborMinesForSquare(squares[row][col])
+            }
+        }
+    }
+    
+    func calculateIsMineLocationForSquare(square: Square) {
+        square.isMineLocation = ((arc4random()%10) == 0) // 1-in-10 chance that each location contains a mine
+    }
+    
+    func calculateNumNeighborMinesForSquare(square : Square) {
+        // first get a list of adjacent squares
+        let neighbors = getNeighboringSquares(square)
+        var numNeighboringMines = 0
+        
+        // for each neighbor with a mine, add 1 to this square's count
+        for neighborSquare in neighbors {
+            if neighborSquare.isMineLocation {
+                numNeighboringMines++
+            }
+        }
+        square.numNeighboringMines = numNeighboringMines
+    }
+    
+    func getNeighboringSquares(square : Square) -> [Square] {
+        var neighbors:[Square] = []
+
+        // an array of tuples containing the relative position of each neighbor to the square
+        let adjacentOffsets =
+            [(-1,-1),(0,-1),(1,-1),
+            (-1,0),(1,0),
+            (-1,1),(0,1),(1,1)]
+        
+        for (rowOffset,colOffset) in adjacentOffsets {
+            // getTileAtLocation might return a Square, or it might return nil, so use the optional datatype "?"
+            let optionalNeighbor:Square? = getTileAtLocation(square.row+rowOffset, col: square.col+colOffset)
+            // only evaluates true if the optional tile isn't nil
+            if let neighbor = optionalNeighbor {
+                neighbors.append(neighbor)
+            }
+        }
+        return neighbors
+    }
+    
+    func getTileAtLocation(row : Int, col : Int) -> Square? {
+        if row >= 0 && row < self.size && col >= 0 && col < self.size {
+            return squares[row][col]
+        } else {
+            return nil
+        }
+    }
+    
+    
 }
--- Mineswifter/Square.swift
+++ Mineswifter/Square.swift
@@ -10,16 +10,18 @@ import Foundation
 
 class Square : NSObject {
     
-    var row:Int
-    var col:Int
+    let row:Int
+    let col:Int
     // give these default values that we will re-assign later with each new game
-    var numNeighboringMines:Int = 0
-    var isMineLocation:Bool = false
-    var isRevealed:Bool = false
+    var numNeighboringMines = 0
+    var isMineLocation = false
+    var isRevealed = false
     
     init(row:Int, col:Int) {
         //store the row and column of the square in the grid
         self.row = row
         self.col = col
+        
+        super.init()
     }
 } 
